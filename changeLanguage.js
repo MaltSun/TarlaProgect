75eb2bad14a924d70fb07a05b916784c9246bf0d
Солодкова Полина <polina.solodkova507@gmail.com>
2024-05-20 10:01:15 +0300

    feat: create js file for changing language

---
+++ changeLanguage.js
@@ -0,0 +1,228 @@
+const i18Obj = {
+  en: {
+    headerShop: "Shop",
+    headerHowItWork: "How it works",
+    headerWhoIsTarla: "Who is tarla",
+    FreshPastryKits: "Fresh pastry kits",
+    Smells:
+      "Bringing the smells and tastes of your favourite patisserie to your home. Our kits have everything you need to easily make killer pastries every time. We’ve done the tricky bits so all you need to do is roll, proof and bake!",
+    Catalog: "Catalog",
+    ChoseYourFav: "Choose your fave",
+    ChoseYourFaveDef:
+      "This sounds easier than it's going to be. We've got all the classics  on here, so now it's over to you.",
+    DingDong: "Ding dong",
+    DingDongDif:
+      "Next you'll get a beautiful little package delivered to your door, on a day and time slot that you choose.",
+    GetBaked: "Get baked",
+    GetBakedDif:
+      "Take your fresh pastry sheet and fillings, learn our pro techniques then never buy a stale pastry ever again.",
+    takeTheCredit: "Take the credit",
+    takeTheCreditDif:
+      "It was all you, nudge-nudge wink-wink. We're just happy to be your loyal side-kick",
+    ReadMore: "Read more",
+    FreshlyBaked:
+      "Freshly baked means fresh bakery smells filling your home, warm oozey pastries on your plates, and never needing to get out of your PJs.",
+    FreshPastry: "Shop pastry kits",
+    Information: "Information",
+    AllButter: "All-Butter Croissant",
+    AllButterComp: "flour, butter, water, milk, sugar, salt, yeast",
+    KitOf6: "Kit of 6 - £13.50",
+    PainAuChocolate: "Pain Au Chocolat",
+    Almond: "Almond Croissant",
+    AlmondComp:
+      "flour, butter, water, milk, sugar, salt, yeast, almonds (whole or sliced), almond extract (optional)",
+    PainAuxPaisins: "Pain Aux Raisins",
+    Raspberry: "Raspberry Danish",
+    RaspberryComp:
+      "flour, butter, sugar, salt, yeast, eggs, milk, raspberry compote or jam, cream cheese or almond paste (optional), powdered sugar (for dusting, optional)",
+    Apricot: "Apricot Danish",
+    SimpleBake: "Simple to bake at home",
+    SimpleBakeDif:
+      "We’ve done the hard work, so you don’t have to. Every pastry is easy to make with our 3-step instructions and doesn’t require any special tools – just an oven!",
+    PlanetFriendly: "Planet-friendly packaging",
+    PlanetFriendlyDif:
+      "We’ve done everything we can to make sure our packaging has the lightest footprint on the planet as possible. We’ve made sure everything is recyclable, and use as little packaging as we can.",
+    BetterThanbakery: "Better than the bakery",
+    BetterThanbakeryDif:
+      "You’ll soon be making pastries to rival the best bakeries in town. We use only the best ingredients for generous pastries that taste and smell amazing. So good, that you’ll take all the credit.",
+    JoinOurCommunity: "Join our community",
+    SendUsMessage: "Send us a message",
+    GetBaked: "and let's get baked",
+    LogIn: "Log in",
+    Price: "Prices",
+    FAQ: "FAQ",
+    Catalog: "Catalog",
+    Location: "Location",
+    ReadMore: "Read more",
+    Privacy: "Privacy Policy",
+    TermsOfService: "Terms of Service",
+    AllButterDiv:
+      "An all-butter croissant is a classic French pastry renowned for its flaky, buttery layers and delicate texture. Its golden exterior hides a soft, airy interior, creating a perfect balance of crispness and tenderness with every bite. This iconic pastry has a rich aroma that fills the air, enticing the senses with its irresistible allure.",
+    PainAuChocolateDif:
+      "Pain au chocolat, also known as a chocolate croissant, is a delectable pastry that combines the buttery, flaky layers of a croissant with rich, indulgent chocolate filling.The dough is then folded over the chocolate, creating multiple layers of pastry enveloping the chocolate filling. After baking, the pain au chocolat emerges from the oven with a golden-brown exterior and a melty, chocolatey interior.",
+    AlmondDif:
+      "An almond croissant is a delectable pastry that starts with the same flaky, buttery layers as a traditional croissant but is then filled with a sweet almond paste or frangipane filling. Once baked, it's topped with a generous sprinkling of sliced almonds, adding a delightful crunch and nutty flavor to every bite.",
+    PainAuxPaisinsDif:
+      " Pain aux raisins, also known as <<raisin Danish>> or <<raisin swirl>>, is a delectable French pastry that showcases layers of buttery, flaky dough wrapped around a sweet filling of pastry cream and plump raisins. The dough is typically rolled out thinly, spread with pastry cream, and sprinkled with raisins before being rolled into a spiral shape.",
+    RaspberryDif:
+      "A raspberry Danish is a delightful pastry that features a tender, flaky dough base, typically shaped into an oval or rectangular form. The dough is then filled with a luscious raspberry compote or jam, which adds a burst of sweet-tart flavor. After baking, the raspberry. Danish is often drizzled with a sweet glaze or dusted with powdered sugar, enhancing its visual appeal and adding a touch of extra sweetness.",
+    ApricotDif:
+      "An apricot Danish is a delightful pastry that combines tender, flaky layers of dough with a sweet and tangy apricot filling. The dough is typically shaped into a square or rectangular form and then filled with apricot jam or compote, which lends a burst of fruity flavor to every bite. Danish is often adorned with a light glaze or dusting of powdered sugar, enhancing its sweetness and visual appeal.",
+    Composition: "Composition",
+    Prices: "Price: kit of 6 - £12",
+    SelfPickup: "Self-Pickup",
+    PickupPoint: "Pickup Point",
+    CourierDelivery: "Courier Delivery",
+    PainAuChocolateComp:
+      "flour, butter, water, milk, sugar, salt, yeast, dark chocolate",
+    PainAuxPaisinsDif:
+      "flour, butter, sugar, salt, yeast, eggs, milk, pastry cream, raisins, powdered sugar (for dusting, optional)",
+    ApricotComp:
+      "flour, butter, sugar, salt, yeast, eggs, milk, apricot jam or compote, cream cheese or almond paste (optional), powdered sugar (for dusting, optional)",
+    CourierDelivery1: "Courier delivery is available within the city.",
+    CourierDelivery2: "Delivery hours: from 12:00 PM to 9:00 PM, Monday to Saturday.",
+    CourierDelivery3: "The delivery cost is 300 rubles.",
+    PickupPoint1:"You can pick up your order at any of our pickup points",
+    PickupPoint2:"Pickup points are located at: 5 Pushkin Street; 20 Sadovaya Street.",
+    PickupPoint3:"Pickup points hours: from 10:00 AM to 8:00 PM, seven days a week.",
+    SelfPickup1:"You can pick up your order at our store located at: 10 Lenin Street.",
+    SelfPickup2:"Self-pickup hours: from 9:00 AM to 6:00 PM, Monday to Saturday",
+    SelfPickup3:"Please call us in advance to confirm your order is ready.",
+    Legend:"Legend",
+    Tarla:"Tarla",
+    Delivery:"Delivery",
+  },
+  ru: {
+    Legend:"Легенда",
+    Tarla:"Тарла",
+    SelfPickup1:"Вы можете забрать свой заказ в нашем магазине, расположенном по адресу: улица Ленина, 10.",
+    SelfPickup2:"Часы самовывоза: с 9:00 утра до 18:00 вечера, с понедельника по субботу",
+    SelfPickup3:"Пожалуйста, позвоните нам заранее, чтобы подтвердить готовность вашего заказа.",
+    PickupPoint1:"Вы можете забрать свой заказ в любом из наших пунктов выдачи.",
+    PickupPoint2:"Пункты самовывоза расположены по адресам: ул. Пушкина, 5; ул. Садовая, 20.",
+    PickupPoint3:"Часы работы пунктов выдачи: с 10:00 утра до 20:00 вечера, семь дней в неделю.",
+    CourierDelivery1: "Возможна курьерская доставка по городу.",
+    CourierDelivery2: "Часы доставки: с 12:00 до 21:00, с понедельника по субботу.",
+    CourierDelivery3: "Стоимость доставки составляет 300 рублей.",
+    SelfPickup: "Самовывоз",
+    PickupPoint: "Пункт выдачи",
+    CourierDelivery: "Доставка курьером",
+    PainAuChocolateComp:
+      "мука, сливочное масло, вода, молоко, сахар, соль, дрожжи, темный шоколад",
+    ApricotComp:
+      "мука, сливочное масло, сахар, соль, дрожжи, яйца, молоко, абрикосовый джем или компот, сливочный сыр или миндальная паста (по желанию), сахарная пудра (для посыпки, по желанию)",
+    PainAuxPaisinsDif:
+      "мука, сливочное масло, сахар, соль, дрожжи, яйца, молоко, кондитерский крем, изюм, сахарная пудра (для посыпки, по желанию)",
+    headerShop: "Ассортимент",
+    headerHowItWork: "Как заказать?",
+    headerWhoIsTarla: "Тарла - это?",
+    FreshPastryKits: "Свежая выпечка",
+    Smells:
+      "Подарите себе дома ароматы и вкусы вашей любимой кондитерской. В наших наборах есть все необходимое, чтобы с легкостью готовить потрясающую выпечку каждый раз. Мы позаботились о самых сложных деталях, поэтому все, что вам нужно сделать, - это раскатать, расстойку и выпекать!",
+    Catalog: "Каталог",
+    ChoseYourFav: "Выберите любимое",
+    ChoseYourFaveDef:
+      "Это звучит проще, чем кажется на самом деле. У нас здесь есть вся классика, так что теперь дело за вами.",
+    DingDong: "Динь-динь",
+    DingDongDif:
+      "Затем вы получите красивую маленькую посылку, доставленную к вашей двери в выбранный вами день и время.",
+    GetBaked: "Поставьте выпекаться",
+    GetBakedDif:
+      "Возьмите свежую форму для выпечки и начинки, изучите наши профессиональные приемы и никогда больше не покупайте черствую выпечку.",
+    takeTheCredit: "Вы виноваты в этом",
+    takeTheCreditDif:
+      "Это все из-за тебя, тычок-тычок, подмигивание-подмигивание. Мы просто счастливы быть твоими верными помощниками",
+    ReadMore: "Читать больше",
+    FreshlyBaked:
+      "Свежая выпечка - это когда ваш дом наполняется запахами свежей выпечки, на ваших тарелках лежит теплая, сочащаяся выпечка, и вам не нужно снимать пижаму.",
+    FreshPastry: "Свежая выпечка",
+    Information: "Информация",
+    AllButter: "Классический круасан",
+    AllButterComp: "мука, сливочное масло, вода, молоко, сахар, соль, дрожжи",
+    AllButterDiv:
+      "Круассаны со сливочным маслом - это классическое французское печенье, известное своими слоеными, маслянистыми слоями и нежной текстурой. За золотистой корочкой скрывается мягкая, воздушная начинка, благодаря чему в каждом кусочке получается идеальный баланс хрустящей корочки и нежности. Это культовое кондитерское изделие обладает насыщенным ароматом, который наполняет воздух, покоряя своим неотразимым очарованием.",
+    KitOf6: "Набор из 6 - £13.50",
+    PainAuChocolate: "Волшебный шоколад",
+    PainAuChocolateDif:
+      "Pain au chocolat, также известный как шоколадный круассан, - это восхитительное кондитерское изделие, в котором маслянистые слоеные слои круассана сочетаются с обильной шоколадной начинкой.Затем тесто укладывается поверх шоколада, образуя несколько слоев теста, которые обволакивают шоколадную начинку. После выпекания пирожное pain au chocolat выходит из духовки с золотисто-коричневой корочкой снаружи и тающим шоколадом внутри.",
+    Almond: "Миндальный круасан",
+    AlmondDif:
+      "Миндальный круассан - это восхитительная выпечка, которая начинается с тех же слоеных, маслянистых слоев, что и традиционный круассан, но затем наполняется пастой из сладкого миндаля или начинкой из франжипана. После запекания его щедро посыпают нарезанным миндалем, придавая каждому кусочку восхитительный хруст и ореховый привкус.",
+    PainAuxPaisins: "Очаравательный изюм",
+    PainAuxPaisinsDif:
+      "Pain aux raisins, также известный как <<изюм по-датски>> или <<изюмный рулет>>, - это восхитительное французское кондитерское изделие, представляющее собой слои маслянистого слоеного теста, обернутые вокруг сладкой начинки из кондитерского крема и сочного изюма. Обычно тесто тонко раскатывают, смазывают кондитерским кремом и посыпают изюмом, прежде чем раскатать в спиральную форму.",
+    Raspberry: "Малина по-датски",
+    RaspberryComp:
+      "мука, сливочное масло, сахар, соль, дрожжи, яйца, молоко, малиновый компот или джем, сливочный сыр или миндальная паста (по желанию), сахарная пудра (для посыпки, по желанию)",
+    RaspberryDif:
+      "Малиновое печенье по-датски - это восхитительная выпечка, которая готовится на основе нежного слоеного теста, обычно овальной или прямоугольной формы. Затем тесто заливается сочным малиновым компотом или джемом, которые придают ему особый сладко-терпкий вкус. После выпечки малиновое печенье. Датский пирог часто поливают сладкой глазурью или посыпают сахарной пудрой, что придает ему дополнительную привлекательность и сладость.",
+    Apricot: "Абрикос по-датски",
+    ApricotDif:
+      "Датское печенье с абрикосами - это восхитительная выпечка, в которой сочетаются нежные, слоеные слоеное тесто со сладкой и острой абрикосовой начинкой. Тесту обычно придают квадратную или прямоугольную форму, а затем заливают абрикосовым джемом или компотом, которые придают фруктовый вкус каждому кусочку. Датский пирог часто украшают легкой глазурью или посыпают сахарной пудрой, что придает ему сладость и внешнюю привлекательность.",
+    SimpleBake: "Просто и вкусно",
+    SimpleBakeDif:
+      "Мы проделали большую работу, так что вам не придется этого делать. С помощью наших пошаговых инструкций каждое тесто легко готовится и не требует специальных инструментов - только духовки!",
+    PlanetFriendly: "Меньше упаковки",
+    PlanetFriendlyDif:
+      "Мы сделали все возможное, чтобы наша упаковка занимала меньше места на планете. Мы позаботились о том, чтобы все было пригодно для вторичной переработки.",
+    BetterThanbakery: "Вкуснее, чем в пекарне",
+    BetterThanbakeryDif:
+      "Мы используем только лучшие ингредиенты для приготовления пышной выпечки, которая обладает потрясающим вкусом и запахом. Она настолько вкусна, что вы будете в восторге.",
+    JoinOurCommunity: "Подпишись",
+    SendUsMessage: "отправить сообщение",
+    GetBaked: "начни печь!",
+    LogIn: "Войти",
+    Price: "Стоимость",
+    FAQ: "Вопросы",
+    Catalog: "Каталог",
+    AlmondComp:
+      "мука, сливочное масло, вода, молоко, сахар, соль, дрожжи, миндаль (цельный или нарезанный ломтиками), миндальный экстракт (по желанию)",
+    Location: "Расположение",
+    ReadMore: "Читать больше",
+    Privacy: "Политика проватности",
+    TermsOfService: "Условия обслуживания",
+    Composition: "Состав",
+    Prices: "Цена: 6 штук - £12",
+    Delivery:"Доставка",
+  },
+};
+
+let currentLanguage = localStorage.getItem("language") || "en";
+
+function getTranslate(lang) {
+  localStorage.setItem("language", lang);
+
+  const elements = document.querySelectorAll("[data-i18]");
+
+  elements.forEach((elem) => {
+    const key = elem.dataset.i18;
+    if (i18Obj[lang][key]) {
+      if (elem.placeholder !== undefined) {
+        elem.placeholder = i18Obj[lang][key];
+      } else {
+        elem.textContent = i18Obj[lang][key];
+      }
+    }
+  });
+}
+
+function applySavedLanguage() {
+  getTranslate(currentLanguage);
+}
+
+document.addEventListener("DOMContentLoaded", applySavedLanguage);
+
+const enButton = document.querySelector('button[data-i18="en"]');
+const ruButton = document.querySelector('button[data-i18="ru"]');
+
+if (enButton) {
+  enButton.addEventListener("click", () => {
+    getTranslate("en");
+  });
+}
+
+if (ruButton) {
+  ruButton.addEventListener("click", () => {
+    getTranslate("ru");
+  });
+}
